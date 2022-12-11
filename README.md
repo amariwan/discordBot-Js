fb6d0831924019beea759104b176f7e5146daf2a
Aland Mariwan <dev@aland-mariwan.de>
2022-12-11 13:36:45 +0100

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+<div align="center">
+        <p><i><b>A Discord for bots</b></i></p>
+</div>
---
+++ index.js
@@ -0,0 +1,67 @@
+const config = require('./config.json');
+const { Discord, Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
+const axios = require('axios');
+const client = new Client({
+	intents: [
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.MessageContent
+	],
+	partials: [ 'MESSAGE', 'CHANNEL', 'REACTION' ]
+});
+
+(async () => {
+	client.on('ready', async () => {
+		console.log(`Logged in as ${client.user.tag}!`);
+		const user = await client.users.fetch('449846575561441280');
+		console.log(user);
+		user.send('Hey ' + user.username);
+		user.send("I'm now online");
+		client.user.setPresence({
+			activities: [ { name: `discord.js v14`, type: `WATCHING` } ],
+			status: 'dnd',
+			game: {
+				name: 'me getting developed',
+				type: 'STREAMING'
+			},
+			afk: false
+		});
+	});
+
+	client.on('warn', (err) => console.warn('[WARNING]', err));
+
+	client.on('error', (err) => console.error('[ERROR]', err));
+
+	client.on('disconnect', () => {
+		console.warn('Disconnected!');
+		process.exit(0);
+	});
+
+	client.on('uncaughtException', (err) => {
+		console.log('Uncaught Exception: ' + err);
+		process.exit(1);
+	});
+
+	client.on('messageCreate', async (message) => {
+		if (message.author.bot) return;
+		const user = await client.users.fetch('449846575561441280');
+		var msg = message.content.toLowerCase();
+		if (msg == 'how are you?') {
+			message.author.send("I'm finde, Thank you " + user.username);
+			message.author.send('what about you ' + user.username);
+		} else {
+			let res = await axios.get(
+				`http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=${encodeURIComponent(msg)}`
+			);
+			message.author.send(res.data.cnt);
+		}
+	});
+
+	client.login(config.token);
+})();
